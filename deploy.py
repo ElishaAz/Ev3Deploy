3ff42450de5caa876ce85af70c518f7152bad7e2
Elisha <elishadar@gmail.com>
2020-10-07 17:19:14 +0300

    Fixes, Cleanups, Added documentation

--- deploy.py
+++ deploy.py
@@ -9,6 +9,8 @@ from paramiko import SSHClient
 from scp import SCPClient
 from typing import List, Optional
 
+from typing.io import TextIO
+
 PATH = './'
 EXECUTABLE = ['*.py', '*.sh']
 PASSWORD = "maker"
@@ -18,74 +20,57 @@ IGNORE_PATH = "./.ignore"
 EXECUTE_FILE = None
 
 
-def read_exclude():
-    if not os.path.exists(Path(IGNORE_PATH)):
-        ignore = open(Path(IGNORE_PATH), 'w+')
+def read_exclude(ignore_path: str) -> List[str]:
+    """
+    Read the exclude file ('.ignore').
+    :param ignore_path: Path to the exclude file.
+    :return: A list of file patterns to ignore.
+    """
+    if not os.path.exists(Path(ignore_path)):
+        ignore = open(Path(ignore_path), 'w+')
         ignore.writelines(['./.ignore\n', './deploy.py\n', '*/.*'])
         ignore.close()
 
-    ignore = open(IGNORE_PATH, 'r')
+    ignore = open(ignore_path, 'r')
     lines = [line.strip() for line in ignore.readlines()]
     return lines
 
 
-def find_dir(root: str = '.', exclude: List[str] = ('*/.*', './deploy.py')) -> List[str]:
+def match(filename: str, patterns: List[str]) -> bool:
     """
-    :param root:
-    :param exclude:
-    :return:
+    Checks if filename matches ANY of 'patterns'.
+    :param filename: A path of a file.
+    :param patterns: A list of standard UNIX file patterns.
+    :return: True if filename matches ANY of 'patterns', False otherwise.
     """
-    res: List[str] = []
-
-    for file in os.listdir(path=root):
-        full_path = root + '/' + file
-        if not match(full_path, exclude):
-            # res.append(full_path)
-            if os.path.isdir(Path(full_path)):
-                # print("DIR", file)
-                res += find_dir(full_path)
-            else:
-                # print("DOC", file)
-                res.append(full_path)
-        else:
-            print("Excluded", file)
-    return res
-
-
-def match(filename: str, matches: List[str]):
-    """
-
-    :param filename:
-    :param matches:
-    :return:
-    """
-    for m in matches:
+    for m in patterns:
         if fnmatch.fnmatch(filename, m):
             return True
     return False
 
 
-def path_join(*path) -> Optional[Path]:
-    if len(path) < 1:
+def path_join(*paths) -> Optional[Path]:
+    """
+    Joins multiple strings to a single Path object.
+    :param paths: paths to join.
+    :return: A Path object corresponding to 'paths'. 'None' if 'paths' is empty.
+    """
+    if len(paths) < 1:
         return None
-    res = None
-    res = Path(path[0]).joinpath(*path[1:])
-    # for p in path:
-    #     if res is None:
-    #         res = Path(p)
-    #     else:
-    #         res.joinpath(p)
-    # print(res)
+    res = Path(paths[0]).joinpath(*paths[1:])
     return res
 
 
-def get_args():
+def get_args() -> None:
+    """
+    Configures command line arguments.
+    """
     global HOSTNAME, USERNAME, PASSWORD, PATH, IGNORE_PATH, EXECUTE_FILE
     parser = argparse.ArgumentParser(description='Send Project to Ev3.')
     parser.add_argument('--hostname', help="The ssh hostname (default is 'ev3dev')")
     parser.add_argument('--username', help="The ssh username (default is 'robot')")
     parser.add_argument('--password', help="The ssh password (default is 'maker')")
-    parser.add_argument('--path', help="The Directory to send (default is working directory).")
+    parser.add_argument('--path', help="The Directory to send (default is current directory).")
     parser.add_argument('--exclude_file',
                         help="The file containing the list of files to ignore (default is '.ignore').")
     parser.add_argument('--execute_file', help="A file to execute after transferring (local path relative to 'PATH').")
@@ -109,71 +94,131 @@ def get_args():
         EXECUTE_FILE = args.execute_file
 
 
-def reroute_stdout(stdout):
-    for l in iter(stdout.readline, ""):
+def reroute_stdout(st: TextIO):
+    """
+    Copies 'st' to system stdout.
+    :param st: An output stream.
+    """
+    for l in iter(st.readline, ""):
         print(l, end="")
 
 
-def reroute_stderr(stderr):
-    for l in iter(stderr.readline, ""):
+def reroute_stderr(st: TextIO):
+    """
+    Copies 'st' to system stderr.
+    :param st: An output stream.
+    """
+    for l in iter(st.readline, ""):
         print(l, end="", file=sys.stderr)
 
 
 run_stdin = True
 
 
-def reroute_stdin(stdin):
+def reroute_stdin(st: TextIO):
+    """
+    Copies system stdin to st.
+    :param st: An input stream.
+    """
+    global run_stdin
     while run_stdin:
-        print(input(), end="", file=stdin)
+        # if sys.stdin.isatty():
+        for line in sys.stdin:
+            if st.closed or sys.stdin.closed or not run_stdin:
+                break
+            print(line, end="", file=st)
+
+
+def deploy(path: str = './', hostname: str = "ev3dev", username: str = "robot", password: str = "maker",
+           execute_file: Optional[str] = None, executable: List[str] = ('*.py',),
+           exclude_path: str = "./.ignore", print_console: bool = True,
+           pipe_stdout: bool = True, pipe_stderr: bool = True, pipe_stdin: bool = False) -> None:
+    """
+    Send code to Ev3
+    :param path: The Directory to send (default is current directory).
+    :param hostname: The ssh hostname (default is 'ev3dev')
+    :param username: The ssh username (default is 'robot')
+    :param password: The ssh password (default is 'maker')
+    :param execute_file: A file to run on the ev3 when finished. 'None' to disable.
+     Note: this file must be marked as executable.
+    :param executable: A list of patterns of files that should be marked as executable (default is ['*.py']).
+    :param exclude_path: The file containing the list of files to ignore (default is '.ignore').
+    :param print_console: Should we print info to the console?
+    :param pipe_stdout: Should we pipe stdout form ev3 to console?
+    :param pipe_stderr: Should we pipe stderr form ev3 to console?
+    :param pipe_stdin: SHould we pipe counsole input to ev3 stdin?
+     This is disabled by default as it cannot terminate without reading from stdin.
+    """
+    # Get / Set working directory
+    os.chdir(path)
+    working_dir = os.getcwd()
+    dir_name = os.path.basename(working_dir)
+
+    exclude = read_exclude(exclude_path)
+
+    # Set up ssh
+    ssh = SSHClient()
+    ssh.load_system_host_keys()
+    ssh.connect(hostname=hostname, username=username, password=password)
+
+    with SCPClient(ssh.get_transport()) as scp:
+        for subdir, dirs, files in os.walk('.'):  # for every file in current working directory:
+            for filename in files:
+                filepath = subdir + '/' + filename  # get full file path (relative to working directory)
+                if match(filepath, exclude):  # if the file path matches any of the excluded patterns:
+                    if print_console:
+                        print('Excluding', Path(filepath))
+                else:
+                    if print_console:
+                        print("Sending", Path(filepath))
+                    # create the directory if it does not exist
+                    ssh.exec_command('mkdir -p ' + path_join('~', dir_name, subdir).as_posix())
+                    # copy files using scp
+                    scp.put(str(path_join(working_dir, filepath)), path_join('~', dir_name, filepath).as_posix())
+
+                    if match(filepath, executable):  # if file path matches any of the executable patterns:
+                        # mark as executable
+                        ssh.exec_command('chmod u+x ' + path_join('~', dir_name, filepath).as_posix())
+
+        if execute_file:
+            if print_console:
+                print(F'\nExecuting {execute_file} ...\n')
+            # execute the file.
+            stdin, stdout, stderr = ssh.exec_command(path_join('~', dir_name, execute_file).as_posix(), get_pty=True)
+
+            # create the rerouting threads
+            if pipe_stdout:
+                out = threading.Thread(target=reroute_stdout, args=(stdout,))
+            if pipe_stderr:
+                err = threading.Thread(target=reroute_stderr, args=(stderr,))
+            if pipe_stdin:
+                sin = threading.Thread(target=reroute_stdin, args=(stdin,))
+
+            # start them
+            if pipe_stdout:
+                out.start()
+            if pipe_stderr:
+                err.start()
+            if pipe_stdin:
+                sin.start()
+
+            # wait for them to terminate
+            if pipe_stdout:
+                out.join()
+            if pipe_stderr:
+                err.join()
+            if pipe_stdin:
+                global run_stdin
+                # tell reroute_stdin to exit without sending data to stdin
+                run_stdin = False
+                sys.stdin.close()
+                sin.join()
+
+            if print_console:
+                print('\nFinished.')
 
 
 if __name__ == '__main__':
     get_args()
 
-    os.chdir(PATH)
-
-    DIR = os.getcwd()
-    DIR_NAME = os.path.basename(DIR)
-
-    # files = find_dir('.', read_exclude())
-    print(read_exclude())
-
-    exclude = read_exclude()
-
-    ssh = SSHClient()
-    ssh.load_system_host_keys()
-    ssh.connect(hostname=HOSTNAME, username=USERNAME, password=PASSWORD)
-
-    with SCPClient(ssh.get_transport()) as scp:
-        for subdir, dirs, files in os.walk('.'):
-            for filename in files:
-                filepath = subdir + '/' + filename
-                if match(filepath, exclude):
-                    print('Excluding', Path(filepath))
-                else:
-                    print("Sending", Path(filepath))
-                    ssh.exec_command('mkdir -p ' + path_join('~', DIR_NAME, subdir).as_posix())
-                    scp.put(str(path_join('.', filepath)), path_join('~', DIR_NAME, filepath).as_posix())
-                    if match(filepath, EXECUTABLE):
-                        ssh.exec_command('chmod u+x ' + path_join('~', DIR_NAME, filepath).as_posix())
-
-        if EXECUTE_FILE:
-            print(F'\nExecuting {EXECUTE_FILE} ...\n')
-            stdin, stdout, stderr = ssh.exec_command(path_join('~', DIR_NAME, EXECUTE_FILE).as_posix(), get_pty=True)
-
-            out = threading.Thread(target=reroute_stdout, args=(stdout,))
-            err = threading.Thread(target=reroute_stderr, args=(stderr,))
-            sin = threading.Thread(target=reroute_stdin, args=(stdin,))
-
-            out.start()
-            err.start()
-            sin.start()
-
-            out.join()
-            err.join()
-
-            run_stdin = False
-
-            sin.join()
-
-            print('\nFinished.')
+    deploy(PATH, PASSWORD, HOSTNAME, USERNAME, EXECUTE_FILE, EXECUTABLE, IGNORE_PATH)
